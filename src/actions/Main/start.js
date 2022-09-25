7f0c1db92203f7a11bd2cc40cc453fbe84c00792
tainio283 <65232786+tainio283@users.noreply.github.com>
2022-09-25 16:29:43 +0300

    Update start.js

--- src/actions/Main/start.js
+++ src/actions/Main/start.js
@@ -2,7 +2,7 @@ const c = require("../../../questions");
 const yesandnoKeyboard = {
   parse_mode: "HTML",
   reply_markup: {
-    keyboard: [["yes", "no"]],
+    keyboard: [["Yes", "No"]],
     resize_keyboard: true,
     one_time_keyboard: true,
   },
