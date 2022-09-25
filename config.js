2ce3ccb1de36e2ab65764d4a64f967a50fdd004f
harish <chandharish198@gmail.com>
2022-09-25 13:26:42 +0530

    first commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "token", // BOt Token
+  token: "5617542620:AAFi49Bh6j16VnPFCVAVIMd5hFjW8HoBZ2E", // BOt Token
   provider_token: "ptoken", // Provider token from botfather for telegram payments
   number: "+123456789", // Your phone number
   payment_amount: 5, // Payment Amount in USD
