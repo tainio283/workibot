7d5249632de056a52e15fa1d97f3f879e4dc8bcc
harish <chandharish198@gmail.com>
2022-09-25 16:45:41 +0530

    first commit

--- src/bot.js
+++ src/bot.js
@@ -11,27 +11,6 @@ require(`./handlers/Commands.js`)(bot, session);
 require(`./handlers/Events.js`)(bot, session);
 require(`./handlers/Actions.js`)(bot, session);
 
-//my add
-
-bot.help((ctx) => {
-  ctx.reply('Send /start to receive a greeting');
-  ctx.reply('Send /keyboard to receive a message with a keyboard');
-  ctx.reply('Send /quit to stop the bot');
-}); bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id);// Context shortcut
-  ctx.leaveChat();
-}); bot.command('keyboard', (ctx) => {
-  ctx.reply(
-    'Keyboard',
-    Markup.inlineKeyboard([
-      Markup.button.callback('First option', 'first'),
-      Markup.button.callback('Second option', 'second'),
-    ])
-  );
-});
-
-
 
 
 bot
