aa6759b84b6e589ff1aa13df783986ef0d706b14
tainio283 <65232786+tainio283@users.noreply.github.com>
2022-09-28 21:01:26 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 module.exports = {
   start: `
-  Hello you guys :)
+  היי, טוב שבאתם :)
   I'm here to help you improve your resume and get more job interviews!
   Please select "Improve an existing file".`,
   button1: "Improve an existing file",
