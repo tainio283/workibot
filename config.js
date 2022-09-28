df6c89e021424cf9fbb456460e1d4931766b4481
tainio283 <65232786+tainio283@users.noreply.github.com>
2022-09-28 16:07:15 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: "5617542620:AAFi49Bh6j16VnPFCVAVIMd5hFjW8HoBZ2E", // BOt Token
   provider_token: "ptoken", // Provider token from botfather for telegram payments
-  number: "+123456789", // Your phone number
+  number: "+972509075188", // Your phone number
   payment_amount: 5, // Payment Amount in USD
 };
